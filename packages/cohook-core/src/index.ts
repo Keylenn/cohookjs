d7b3c6d6c5bbf7d0a0cce3eabd6c4aecccf967ad
keylenn <darklight_on@163.com>
2021-09-23 21:15:01 +0800

    feat: export immer plugin

--- packages/cohook-core/src/index.ts
+++ packages/cohook-core/src/index.ts
@@ -2,5 +2,7 @@ import createContainer from "./core/createContainer"
 
 export * from "./types"
 
+export { enableAllPlugins, enableES5, enablePatches, enableMapSet } from "immer"
+
 export { createContainer }
 export default createContainer
